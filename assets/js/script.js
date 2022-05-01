4e72d4d873d7feccb397327bbb448b804ed5fb14
Jessica <98659450+JessicaSaantos@users.noreply.github.com>
2022-05-01 15:47:52 -0300

    Aplicando efeitos com scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,17 @@ for (const link of links ) {
         nav.classList.remove('show')
     })
 }
+
+/* Alteração no header da pgn quando ocorrer o scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll' , function(){
+    if (window.scrollY >= navHeight) {
+        //scroll é maior que a altura do header
+        header.classList.add('scroll')
+    }
+    else{
+        header.classList.remove('scroll')
+    }
+})
